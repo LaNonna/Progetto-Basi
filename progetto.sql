d9e92013596f5c80d257bbd76cf43552728dbbdd
LaNonna <matteoumatche@gmail.com>
2022-05-11 13:08:41 +0200

    aggiornato il file con riempimento fino a Batch

--- progetto.sql
+++ progetto.sql
@@ -54,7 +54,7 @@ create table Dipendente{
 	citta 			varchar[20] 		not null,
 	via 			varchar[50] 		not null,
 	civico 		varchar[5] 		not null,
-	CAP 			char[5] 		not null,
+	CAP 			char[5]								default null,
 	stato 			varchar[3] 		not null					default "IT",
 	stipendio_mensile	numeric(9,2) 		not null					default 0,
 	telefono 		varchar[13]
@@ -139,7 +139,7 @@ create table TransazioneUscente{
 }engine=innodb default charset=latin1;
 
 create table Ricetta{
-	n_ricetta 		char[16]  	auto_increment primary key,
+	n_ricetta 		char[16]  	auto_increment		primary key,
 	tipo 			varchar[10] 	check() 						default "mah",
 	data_scadenza 		date 									default null,
 	CF_medico 		char[16]		not null,
@@ -387,44 +387,76 @@ create table Effettuazione{
 
 
 insert into Dipendente(CF,email,data_nascita,nome,cognome,sex,citta,via,civico,CAP,stato,stipendio_mensile,telefono) values
-('ZNZDZN42S10E071F','costanzino@gmail.com',1-01-1990,'costanzo','vecchio','M','Padova','Roma','2B','35020','IT',2000,'+398887760093')
-('ZTPCNY52H51E639P','franceschella@gmail.com',5-060-2022,'francesca','petrella','F','Albignasego','Guizza','441','35021','IT',1700,'+3935126265555')
-('HNTHRZ65S62E048Z','paolo.rosso@gmail.com','paolo',17-11-1977,'rossi','M','roma','verdi','4','10452','IT',2500,'+391234567895')
-('CXFCRD88P59B285H','massimo.pericolo@gmail.com',23-10-1995,'massimo','pericolo','M','Milano', 'navigli','1A','96857','IT',2000,'+399876543211')
-('FCTNBT60A66E625C','mammamia@gmail.com','maria',09-02-2000,'annoiata','F','Bari','S.Antono'.'44','52526','IT',4500,'+3977778888621')
-('LKNHDD37T45C082I','gianfrancioschio@gmail.com',02-02-1982,'gian','francioschio','A','Roma','dei martiri','9','IT','10452',5000, null)
-('YZCLHH46D26E182P','prigioniero@libero.it',,)
-('GYNQKD39A26A745S','fredda.laura@hotmail.com',,)
-('TSWWHP49M70I661Z','aiuto.chenoia@gmail.com',,)
-('CHBBQT80R45A266E','alessandrino99@libero.it',,)
-('CDCRHT98E56F058Y','franca paolini@hotmail.com',,)
-('HBBGFR61H17A692E','bassi.maestro@gmail.com',,)
-('MTCMTT98R16G224G','matteoumatche@gmail.com',6-10-1998,'Matteo','Umatche','M','Cartura','A.Vivaldi','5','35025','IT',5000,'+393888766242');
-()
-()
-()
-()
-()
+('ZNZDZN42S10E071F','costanzino@gmail.com',		01-01-1990,'costanzo','vecchio','M',		'Padova','Roma','2B','35020','IT',		2000,	'+398887760093'),/*farmacista pd*/
+('ZTPCNY52H51E639P','franceschella@gmail.com',	5-060-2022,'francesca','petrella','F',	'Albignasego','Guizza','441','35021','IT',	1700,	'+3935126265555'),/*magazziniere pd*/
+('HNTHRZ65S62E048Z','paolo.rosso@gmail.com',		17-11-1977,'paolo','rossi','M',		'Roma','verdi','4','10452','IT',		2500,	'+391234567895'),/*farmacista ro*/
+('CXFCRD88P59B285H','massimo.pericolo@gmail.com',	23-10-1995,'massimo','pericolo','M',		'Milano', 'navigli','1A','96857','IT',	2000,	'+399876543211'),/*impiegato mi*/
+('FCTNBT60A66E625C','mammamia@gmail.com','maria',	09-02-2000,'madre','addolorata','F',		'Bari','S.Antono'.'44','52526','IT',		4500,	'+3977778888621'),/*direttore ba*/
+('LKNHDD37T45C082I','gianfrancioschio@gmail.com',	02-02-1982,'gian','francioschio','A',		'Roma','dei martiri','9','IT','10452',	5000,	null),		 /*direttore ro*/
+('YZCLHH46D26E182P','prigioniero@libero.it',		05-12-1988,'giulia','jail','F',		'Milano','napoleone','8bis','96857','IT',	2000,	'+395665897432'),/*direttore mi*/
+('GYNQKD39A26A745S','fredda.laura@hotmail.com',	08-09-2001,'laura','fredda','F',		'Lugano','wurstel','71',null,'SW',		1700,	'+405698327416'),/*farmacista mi*/
+('TSWWHP49M70I661Z','aiuto.chenoia@gmail.com',	08-04-2003,'aiuto','chenoia','M',		'Bari','cannolo','7','52526','IT',		1700,	'+398527416543'),/*magazziniere ba*/
+('CHBBQT80R45A266E','alessandrino99@libero.it',	25-12-1999,'alessandro','magno','M',		'Roma','caduti','101','10452','IT',		1500,	'+396664875139'),/*magazziniere ro*/
+('CDCRHT98E56F058Y','franca.paolini@hotmail.com',	18-01-1984,'franca','petrella','F',		'Cagliari','mirto','6T','55555','IT',		5000,	'+399832741965'),/*direttore ca*/
+('HBBGFR61H17A692E','bassi.maestro@gmail.com',	13-06-1990,'davide','bassi','M',		'Pecorinia','pecora','20','IT','55554',	1600,	'+393298765419'),/*farmacista ca*/
+('MTCMTT98R16G224G','matteoumatche@gmail.com',	06-10-1998,'Matteo','Umatche','M',		'Cartura','A.Vivaldi','5','35025','IT',	5000,	'+393888766242'),/*direttore pd*/
+('WTZZBV34M01F176K','popefrancis@church.com',		01-01-1950,'francesco ','bergoglio','A',	'citta del vaticano','piazza san pietro','0',null,'VC',1500,'+390000000000'),/*magazziniere ro*/
+('BFNZSR60B45D819Z','spiderman@marvel.com',		31-01-1973,'peter','parker','M',		'new cartura','ragno','15',35022,'IT',	2500,	'+395555588862'),/*farmacista*/
+('JLHDFB72S49G025O','lovwpljchow@gmail.com',		09-02-1986,'caso','random','F',		'uihyf','qwert','678','95421','IT',		0036,	'+393333333332'),/*impiegato ca*/
+('CCMQMK30S15L890M','giangi.pongi@hotmail.com',	19-11-2000,'gianluca','pongi','M',		'arancinia','fritta','4',52522,'IT',		2200,	'+398787564123'),/*farmacista ca*/
+('QZLCDH52P11A494T','marioi.rossi@libero.it',		22-12-1991,'mario','rossi','M',		'mirtopoli','bicchiere','1','55549','IT'	1500,	'+391245789633');/*magazziniere ca*/
 
 insert into Farmacista(CF_farmacista) values
+('ZNZDZN42S10E071F'),
+('HNTHRZ65S62E048Z'),
+('GYNQKD39A26A745S'),
+('HBBGFR61H17A692E'),
+('BFNZSR60B45D819Z'),
+('CCMQMK30S15L890M');
 
 insert into Magazziniere(CF_magazziniere) values
+('ZTPCNY52H51E639P'),
+('TSWWHP49M70I661Z'),
+('WTZZBV34M01F176K'),
+('CHBBQT80R45A266E'),
+('QZLCDH52P11A494T');
 
 insert into Impiegato(CF_Impiegato,is_direttore) values
+('LKNHDD37T45C082I','1'),
+('CXFCRD88P59B285H','0'),
+('FCTNBT60A66E625C','1'),
+('MTCMTT98R16G224G','1'),
+('JLHDFB72S49G025O','0');
 
-insert into Sede(id_sede,citta,via,civico,CAP,stato,telefono) values
+insert into Sede(citta,via,civico,CAP,stato,telefono) values
+('Padova','piazza delle erbe','1','35020','IT','0499555880'),
+('Roma','colosseo','50','10452','IT','049555506'),
+('Cagliari','lungomare','14','55555','IT','0499555990'),
+('Bari','piazza arancino','7','52526','IT','0499555762');
 
 insert into Magazzino(id_magazzino) values
+(1),		/*Padova*/
+(2),		/*Roma*/
+(3),		/*Cagliari*/
+(4);		/*Bari*/
 
 insert into Batch(codice_batch) values
+('0000000000000000'),('1616161616161616'),
+('0101010101010101'),('8888888888888888'),
+('1010101010101010'),('6666666666666666'),
+('6565656565656565'),('94gh9i4uh5guh4ih'),
+('5125125125125125'),('8927gyhikhbi2hic'),
+('8963254715256398'),('h986rfouio7t5i79'),
+('1111111111111111'),('897t8gi87yg8u789'),
+
 
 insert into Prodotto(codice_prodotto,nome,marca,prezzo_unitario) values
 
 insert into ProdottoDeperibile(codice_prodotto,data_scadenza,p_attivo,p_fascia,r_necessaria) values
 
-insert into Ricevuta(n_ricevuta,timestamp_r) values
+insert into Ricevuta(timestamp_r) values
 
-insert into TransazioneUscente(n_transazione,tmestamp_t,importo) values
+insert into TransazioneUscente(timestamp_t,importo) values
 
 insert into Fornitura(id_fornitura) values
 
